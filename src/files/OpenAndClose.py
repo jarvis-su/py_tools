a5360b3c1894bcbf865c60c69d6190727c26ce99
jarvis-su001 <jarvis_su@126.com>
2017-04-13 11:41:49 +0800

    Init the package

--- src/files/OpenAndClose.py
+++ src/files/OpenAndClose.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 f = open('E:/tmp/test.txt')
-print ('文件名：', f.name)
-print ('是否处于关闭状态：', f.closed)
-print ('打开的模式：', f.mode)
+print('文件名：', f.name)
+print('是否处于关闭状态：', f.closed)
+print('打开的模式：', f.mode)
 f.close()
-print ('是否处于关闭状态：', f.closed)
+print('是否处于关闭状态：', f.closed)
