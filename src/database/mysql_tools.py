0841365233f8e7568733b6342d409bfef437b1de
JarvisSu <Jarvis_su@126.com>
2018-03-03 19:19:01 +0800

    add a demo file to connect MYSQL

---
+++ src/database/mysql_tools.py
@@ -0,0 +1,23 @@
+import  pymysql
+
+
+db_host = "47.95.235.115"
+db_user = "jarvis_db"
+db_user_pwd = "Jarvis%123456"
+db_database = "jarvis_db"
+
+db = pymysql.connect(db_host,db_user,db_user_pwd,db_database)
+
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+
+# 使用 execute()  方法执行 SQL 查询
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+
+print("Database version : %s " % data)
+
+# 关闭数据库连接
+db.close()
